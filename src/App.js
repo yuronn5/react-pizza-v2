04dcc87b2a3419d0af070a011433ee0c4ec728c8
yurii bilous <yurii.b@elogic.com.ua>
2023-08-02 12:22:35 +0300

    react router links, use Link exept ahref to link to pages

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
 import './scss/app.scss';
+
+
 import React, {useEffect, useState} from "react";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
 
 function App() {
+    // const pathname = window.location.pathname;
 
-
-  return (
-      <div className="wrapper">
-        <Header />
-        <div className="content">
-          <div className="container">
-              <Home />
-          </div>
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <Routes>
+                        <Route path='/' element={<Home/>}/>
+                        <Route path='/cart' element={<Cart/>}/>
+                        <Route path='*' element={<NotFound/>}/>
+                    </Routes>
+                </div>
+            </div>
         </div>
-      </div>
-  );
+    );
 }
 
 export default App;
